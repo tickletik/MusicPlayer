346de54fbbeacd8840b0cbc66d3ecc28a665468a
Ronny Abraham <ronny.coder @gmail.com>
2017-12-21 14:16:32 +0200

    add playButton action

--- MusicWireframe/Controllers/ViewController.swift
+++ MusicWireframe/Controllers/ViewController.swift
@@ -47,6 +47,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func playButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
