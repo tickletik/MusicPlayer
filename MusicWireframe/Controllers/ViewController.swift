64607e1ad0b8b625c1af515ae073199323f62c10
Ronny Abraham <ronny.coder @gmail.com>
2017-12-21 14:34:20 +0200

    when button released, re-expand button background and dissappear it

--- MusicWireframe/Controllers/ViewController.swift
+++ MusicWireframe/Controllers/ViewController.swift
@@ -69,6 +69,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchedUpInside(_ sender: UIButton) {
+        var buttonBackground: UIView
+        
+        switch sender {
+        case reverseButton:
+            buttonBackground = reverseBackground
+        case playPauseButton:
+            buttonBackground = playPauseBackground
+        case forwardButton:
+            buttonBackground = forwardBackground
+        default:
+            return
+        }
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            buttonBackground.alpha = 0.0
+            buttonBackground.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            sender.transform = CGAffineTransform.identity
+        }) { (_) in
+            buttonBackground.transform = CGAffineTransform.identity
+        }
     }
     
     @IBAction func playButtonTapped(_ sender: UIButton) {
