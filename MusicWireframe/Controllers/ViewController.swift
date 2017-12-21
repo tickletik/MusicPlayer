141669373c4e29d7700817b80dac13b78c94c133
Ronny Abraham <ronny.coder @gmail.com>
2017-12-21 12:28:01 +0200

    fixed order of backgrounds, put all backgrounds into outlet collection

--- MusicWireframe/Controllers/ViewController.swift
+++ MusicWireframe/Controllers/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var forwardBackground: UIView!
     @IBOutlet weak var playPauseBackground: UIView!
     
+    @IBOutlet var backgrounds: [UIView]!
+    
     @IBOutlet weak var reverseButton: UIButton!
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var forwardButton: UIButton!
@@ -22,6 +24,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        reverseBackground.layer.cornerRadius = 35.0
+        reverseBackground.clipsToBounds = true
+        
+        playPauseBackground.layer.cornerRadius = 35.0
+        playPauseBackground.clipsToBounds = true
+        
+        forwardBackground.layer.cornerRadius = 35.0
+        forwardBackground.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
